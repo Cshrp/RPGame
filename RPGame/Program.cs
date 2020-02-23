d831b1f7b4a875890a97ee47e23a2c00fde7c5c3
Cshrp <61374998+Cshrp@users.noreply.github.com>
2020-02-23 11:44:16 +0300

    RPGame
    Характеристики персонажа

---
+++ RPGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpecificPerson myPers = new SpecificPerson();
+            myPers.health = 100;
+            myPers.level = 1;
+            myPers.money = 10;
+            myPers.exp = 0;
+            myPers.force = 1;
+            
+        }
+    }
+}
---
+++ RPGame/SpecificPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGame
+{
+    class SpecificPerson
+    {
+        public int health;
+        public int force;
+        public int money;
+        public int level;
+        public int exp;
+    }
+}
